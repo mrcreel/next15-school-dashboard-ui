c5b021e2e72cf6d313ac6c06c8a90e9455048088
Michael R Creel <michaelrcreel@gmail.com>
2024-12-14 16:27:21 -0600

    Finish dashboard layout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,12 +4,12 @@ const NavBar = () => {
   return (
     <div className="flex items-center justify-between p-4">
       {/* Search Bar */}
-      <div className="hidden md:flex">
+      <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
         <Image src="/search.png" alt="search" height={14} width={14} />
-        <input type="text" name="" id="" className="" placeholder="Search..." />
+        <input type="text" name="" id="" className="w-[200px] p-2 bg-transparent outline-none" placeholder="Search..." />
       </div>
       {/* Icons and User */}
-      <div className="flex items-center gap-6">
+      <div className="flex items-center gap-6 w-full justify-end">
         <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
           <Image src="/message.png" alt="messages" height={20} width={20} />
         </div>
